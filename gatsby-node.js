979fd277caca21ae413e8cbfd09d655c3588d638
l3ff4n0 <frasson.stefano@gmail.com>
2022-07-16 17:26:27 +0200

    Added on gastby node file createPages API to create dynamically the page templates.  (To test with menu) Addded also the template pages

---
+++ gatsby-node.js
@@ -0,0 +1,69 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+// You can delete this file if you're not using it
+const path = require(`path`)
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+  const BlogPostTemplate = path.resolve("./src/templates/BlogPost.js")
+  const PageTemplate = path.resolve("./src/templates/page.js")
+
+  const result = await graphql(`
+    {
+        allWpPost {
+        edges {
+          node {
+            slug
+            id
+          }
+        }
+      }
+      allWpPage {
+        edges {
+          node {
+            template {
+              templateName
+            }
+            isFrontPage
+            slug
+            status
+            title
+            link
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+
+  const BlogPosts = result.data.allWpPost.edges
+  BlogPosts.forEach(post => {
+    createPage({
+      path: `/post/${post.node.slug}`,
+      component: BlogPostTemplate,
+      context: {
+        id: post.node.id,
+      },
+    })
+
+    const Pages = result.data.allWpPage.edges
+    Pages.forEach(page => {
+      const tplPage = (page.node.isFrontPage) ? path.resolve("./src/templates/FrontPage.js") : path.resolve("./src/templates/"+ page.node.template.templateName.replace(/\s/g, '') +".js")   
+      createPage({
+        path: `/${page.node.slug}`,
+        component: tplPage,
+        context: {
+          id: page.node.id,
+        },
+      })
+    })
+  })
+}
---
+++ src/templates/FrontPage.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Header from "../components/header/header"
+import Main from "../components/main/main"
+import Footer from "../components/footer/footer"
+
+const DefaultPge = ({ data }) => {
+  return (
+    <div> 
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  )
+}
+
+export default DefaultPge
